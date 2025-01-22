91316dd4ea196f056b3b7c6809c8e9bd572b0f16
Maria_Prokhorova <luchiksolnza@gmail.com>
2025-01-22 22:12:03 +0700

    Initial commit

---
+++ main/java/pro/sky/java/course2/course2_ApplicationForExam/Course2ApplicationForExamApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.course2_ApplicationForExam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course2ApplicationForExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course2ApplicationForExamApplication.class, args);
+	}
+
+}
---
+++ test/java/pro/sky/java/course2/course2_ApplicationForExam/Course2ApplicationForExamApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.course2_ApplicationForExam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course2ApplicationForExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
